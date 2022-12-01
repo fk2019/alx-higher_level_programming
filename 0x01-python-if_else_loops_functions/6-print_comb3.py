c5b41878dc40687a8d7c3d128e9dad9ec9d1bfab
fk2019 <fkmuiruri8@gmail.com>
2022-12-01 03:46:38 -0800

    Edit func

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for n in range(i+1, 10):
-        print(f"{i}{n}",end="")
-        if i != 8 or n != 9:
-            print(f", ", end="")
-print(f"\n")
+        print(f"{i}{n}", end="")
+        print("{}".format(", " if i != 8 or n != 9 else "\n"), end="")
