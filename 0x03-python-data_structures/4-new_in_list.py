bab904ab6c6bc40cf2dfe2b7ce6071f38978109f
fk2019 <fkmuiruri8@gmail.com>
2022-12-04 08:41:32 -0800

    remove whitespace around operator

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
         return (my_list)
     elif idx > len(my_list) - 1:
         return (my_list)
-    new_list=my_list.copy()
+    new_list = my_list.copy()
     new_list[idx] = element
     return (new_list)
