e8624c7237830a69705b710bbfecc84da898ff23
fk2019 <fkmuiruri8@gmail.com>
2022-12-05 12:42:26 -0800

    Add 0-printlist

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+    idx = len(my_list)
+    while (idx > 0):
+        print("{}".format(idx))
+        idx -= 1
