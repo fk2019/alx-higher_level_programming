d1a678f95752b4fb021eccdd155e1851b84a9c59
fk2019 <fkmuiruri8@gmail.com>
2022-12-06 01:39:46 -0800

    Add 5-no-c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    my_string = "".join([c for c in my_string if (c != "c")])
+    my_string = "".join([c for c in my_string if (c != "C")])
+    return (my_string)
