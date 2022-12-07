6cf39bf095c37b3bb1cb78acce3cb3259911aab7
fk2019 <fkmuiruri8@gmail.com>
2022-12-07 09:05:47 -0800

    Revert func

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,24 @@
 #include "lists.h"
+#include <stdio.h>
 
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: pointer to a pointer of the start of the list
+ * @n: integer to be included in node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+        listint_t *new;
+
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
+                return (NULL);
+        new->n = n;
+        new->next = *head;
+        *head = new;
+        return (new);
+}
 /**
  * is_palindrome - checks if singly linked list is a palindrome
  * @head: pointer to a pointer of the start of the list
@@ -8,40 +27,49 @@
  */
 int is_palindrome(listint_t **head)
 {
-        listint_t *current;
+        listint_t *current, *new;
+        int i, j;
 
         current = *head;
-        int i, k, j;
 
+        new = NULL;
         i = 0;
         while (current != NULL)
         {
                 current = current->next;
                 i++;
         }
+        j = i / 2;
+        i = 0;
         current = *head;
-        int s[i];
-        j = 0;
+        while (i < j)
+        {
+                current = current->next;
+                i++;
+        }
         while (current != NULL)
         {
-                s[j++] = current->n;
+                add_nodeint(&new, current->n);
                 current = current->next;
         }
-        int p[i];
-        k = 0;
-        j = i-1;
-        while (k < i)
+
+        current = *head;
+        while (new->next != NULL && current->next != NULL)
         {
-                p[k] = s[j];
-                k++;
-                j--;
-        }
-        j = 0;
-        while (j < i)
-        {
-                if (s[j] != p[j])
+                if (current->n != new->n)
+                {
+                        free_listint(new);
                         return (0);
-                j++;
+                }
+                current = current->next;
+                new = new->next;
         }
+        if (current->n != new->n)
+        {
+                free_listint(new);
+                return (0);
+        }
+        free_listint(new);
         return (1);
 }
+
