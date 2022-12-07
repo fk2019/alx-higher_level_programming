2f09c119290200a53d3f38d5729b09f7acebf472
fk2019 <fkmuiruri8@gmail.com>
2022-12-07 09:08:54 -0800

    Edit func

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     idx = 0
-    while (idx < len(my_list) + 1):
-        print("{}".format(idx))
+    while (idx < len(my_list)):
+        print("{}".format(my_list[idx]))
         idx += 1
