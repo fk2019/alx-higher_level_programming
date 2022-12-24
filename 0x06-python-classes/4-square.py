763ee65687e22cf6ba9c7008af5ecf142ce78245
fk2019 <fkmuiruri8@gmail.com>
2022-12-24 04:03:43 -0800

    Add 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This moduel defines a class Square"""
+
+
+class Square():
+    """This class defines a square"""
+    def __init__(self, size=0):
+        """Initialize class
+        Args:
+            size: First parameter
+
+        Raises:
+            TypeError: Size must be an int
+            ValueError: Size must be >= 0
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Returns size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns area of square"""
+        return self.__size**2
