6e32fb44ea60b4692ab57bf7f26cec0c0c5920c7
fk2019 <fkmuiruri8@gmail.com>
2022-12-24 03:30:30 -0800

    Add 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This moduel defines a class Square"""
+
+
+class Square():
+    """This class defines a square"""
+    def __init__(self, size):
+        """Initialize class
+        Args:
+            size: First parameter
+        """
+        self.__size = size
