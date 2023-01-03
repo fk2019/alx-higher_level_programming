b861781a8463f984c3e0f9b4647c1689a7a1e843
fk2019 <fkmuiruri8@gmail.com>
2023-01-03 09:52:26 -0800

    Align methods

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,30 +16,30 @@ class Rectangle():
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """Returns width"""
-            return self.__width
+    @property
+    def width(self):
+        """Returns width"""
+        return self.__width
 
-        @property
-        def height(self):
-            """Returns height"""
-            return self.__height
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
-        @width.setter
-        def width(self, value):
-            if type(width) != int:
-                raise TypeError("width must be an integer")
-            if width < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
+    @property
+    def height(self):
+        """Returns height"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            if type(height) != int:
-                raise TypeError("height must be an integer")
-            if height < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = value
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
