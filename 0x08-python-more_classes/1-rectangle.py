b5855a66af9da193dfc1fe4d8b36555a85d7345c
fk2019 <fkmuiruri8@gmail.com>
2023-01-03 08:49:22 -0800

    Add exceptions in init

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,18 @@ class Rectangle():
             TypeError: width must be an integer, height must be an integer
             ValueError: width must be >= 0, height must be >= 0
         """
-        self.width = width
-        self.height = height
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
 
         @property
         def width(self):
