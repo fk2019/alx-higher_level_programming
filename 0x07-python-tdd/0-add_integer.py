01e2fd2e2988f57b9a5e90c425e4cb44b82933b5
fk2019 <fkmuiruri8@gmail.com>
2023-01-05 09:05:14 -0800

    Add tests/ and 0-add_int.py

---
+++ 0x07-python-tdd/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This function adds two integers
+
+Args:
+    a: First parameter
+    b: Second parameter
+
+Raises
+    TypeError: a must be an integer, b must be an integer
+"""
+
+def add_integer(a, b=98):
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
