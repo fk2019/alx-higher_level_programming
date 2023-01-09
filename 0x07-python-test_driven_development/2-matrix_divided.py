28b55ce71707fba5b48e4ab04f84f68fd7af4947
fk2019 <fkmuiruri8@gmail.com>
2023-01-09 11:28:28 -0800

    Add 2-matrix_divided files

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module defines a matrix_divided function
+
+Args:
+    matrix: matrix(list of lists)
+    div: divisor
+
+Raises:
+    TypeError: matrix must be a matrix (list of lists) of integers/floats
+    TypeError: div must be a number
+    TypeError: Each row of the matrix must have the same size
+    ZeroDivisionError: division by zero
+"""
+
+
+def matrix_divided(matrix, div):
+    """Divides matrix by div and return a new matrix"""
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    for i in matrix:
+        if type(i) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if len(matrix[0]) != len(matrix[1]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for j in i:
+            if type(j) not in [int, float]:
+                raise TypeError("matrix must be a matrix (list of lists) of\
+                integers/floats")
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(i / div, 2) for i in j] for j in matrix]
