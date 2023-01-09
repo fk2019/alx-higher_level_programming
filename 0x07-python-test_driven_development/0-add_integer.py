d984f9df4e6cdf19ef371b3726275675edfe2e00
fk2019 <fkmuiruri8@gmail.com>
2023-01-09 12:48:21 -0800

    Edit function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,4 +16,6 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float] or b is None:
         raise TypeError("b must be an integer")
+    if type(int(a)) is not int or type(int(b)) is not int:
+        raise ValueError("cannot convert float NaN to integer")
     return (int(a) + int(b))
