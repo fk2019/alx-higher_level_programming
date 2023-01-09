c671e3d1f06307feb6a5b7e8acfd0c874940c5f1
fk2019 <fkmuiruri8@gmail.com>
2023-01-09 12:37:00 -0800

    Add more tests

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -31,9 +31,13 @@ def matrix_divided(matrix, div):
             if type(j) not in [int, float]:
                 raise TypeError("matrix must be a matrix (list of lists) of\
  integers/floats")
+            if type(int(j)) is not int:
+                raise ValueErrror("cannot convert float NaN to integer")
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    if type(int(div)) is not int:
+        raise ValueError("cannot convert float float NaN to integer")
 
     return [[round(i / div, 2) for i in j] for j in matrix]
