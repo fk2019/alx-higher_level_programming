e9851e5491ae3145f9db4354e5d0470bac62a87b
fk2019 <fkmuiruri8@gmail.com>
2023-01-10 22:44:57 -0800

    Remove circles.py

--- 0x07-python-test_driven_development/circles.py
+++
@@ -1,9 +0,0 @@
-from math import pi
-
-def circle_area(r):
-    if type(r) not in [int, float]:
-        raise TypeError("radius must a non-negative real number")
-    if r < 0:
-        raise ValueError("radius must be a positive number")
-    return pi * (r ** 2)
-
