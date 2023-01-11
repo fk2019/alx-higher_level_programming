d16cd7ebb4dee4eddb90f3806da6ba71849ceeb7
fk2019 <fkmuiruri8@gmail.com>
2023-01-11 10:03:01 -0800

    Edit comments

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,8 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """Returns True of obj is instance of a_class that may also inherit from specified class"""
+    """Returns True of obj is instance of a_class that may also inherit \
+    from specified class"""
     if isinstance(obj, a_class):
         return True
     return False
