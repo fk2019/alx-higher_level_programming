7d94bfba860ec777e85f56e392b7224b1ca95b4b
fk2019 <fkmuiruri8@gmail.com>
2023-01-11 10:01:25 -0800

    Add 3 file

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module defines function that checks instance of a class object
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True of obj is instance of a_class that may also inherit from specified class"""
+    if isinstance(obj, a_class):
+        return True
+    return False
