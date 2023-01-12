af2c90d92fe423f3e1cd8d667f7a5824a45b862b
fk2019 <fkmuiruri8@gmail.com>
2023-01-12 02:19:28 -0800

    Add 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module defines Square class and inherits Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class inherits from Rectangle class"""
+    def __init__(self, size):
+        """Initialize the class"""
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+        self.integer_validator("size", self.__size)
+
+    def __str__(self):
+        """Return string"""
+        return ("[{}] {}/{}".format(type(self).__name__, self.__size,
+                                    self.__size))
