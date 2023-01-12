3ac6e8ba5bfe17b6ba40d07febe04a78ce558172
fk2019 <fkmuiruri8@gmail.com>
2023-01-11 22:10:31 -0800

    Add 6-base

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""""Module defines BaseGeometry class
+"""
+
+
+class BaseGeometry():
+    """This class raises an Exception"""
+    def area(self):
+        """Raise Exception"""
+        raise Exception("area() is not implemented")
