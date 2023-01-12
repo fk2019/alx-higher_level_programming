53577775d411da0cfa7ef8222294e7bd0dda4c7b
fk2019 <fkmuiruri8@gmail.com>
2023-01-12 11:22:39 -0800

    Add 0-file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module reads a text file and prints to stdout
+"""
+
+
+def read_file(filename=""):
+    """Prints file contents to stdout"""
+    with open(filename, encoding='utf-8') as f:
+        print(f.read())
