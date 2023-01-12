ee905921ad37a0d2747cd64b9aae8e9bf0fa1c1c
fk2019 <fkmuiruri8@gmail.com>
2023-01-11 23:06:21 -0800

    Add 7-bg files

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""""Module defines BaseGeometry class
+"""
+
+
+class BaseGeometry():
+    """This class raises an Exception"""
+    def area(self):
+        """Raise Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
