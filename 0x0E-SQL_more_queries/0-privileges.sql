9dff304f3262df2d16a88c9f4117a326871d78e4
fk2019 <fkmuiruri8@gmail.com>
2023-02-18 10:41:12 -0800

    Edit user name

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- list all privileges for user_0d_0 and user_0d_02
-SHOW GRANTS FOR 'user_0d_01'@'localhost';
-SHOW GRANTS FOR 'user_0d_02'@'localhost';
+-- list all privilenges for user_0d_0 and user_0d_02
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
