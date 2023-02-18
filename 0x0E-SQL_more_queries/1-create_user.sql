289b131386d1219b0ff9b351e24260d055272929
fk2019 <fkmuiruri8@gmail.com>
2023-02-18 10:41:19 -0800

    Edit user name

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create user user_0d_01 with all privileges with user_0d_01_pwd as pass
-CREATE USER IF NOT EXISTS 'user_0d_01'@'localhost' IDENTIFIED BY 'user_0d_01_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_01'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
