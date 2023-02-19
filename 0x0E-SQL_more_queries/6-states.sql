a95852c29ffc4b9dd34bc890de5dcee0c7567573
fk2019 <fkmuiruri8@gmail.com>
2023-02-18 23:03:09 -0800

    Add states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- create db hbtn_0d_usa and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(256)
+);
