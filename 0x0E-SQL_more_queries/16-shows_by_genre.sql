89e98b1d2c9d52035a2814156bf0bf585284c286
fk2019 <fkmuiruri8@gmail.com>
2023-02-19 02:03:03 -0800

    Add shows_by_genre

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- list all shows and genres to that show from db hbtn_0d_tvshows
+-- display tv_shows.title, tv_genres.name
+-- display NULL if show lacks genre
+-- sort in ascending order by show and genre
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
