5040d7ed448fc505a2fee2137af3b1effcd4547f
fk2019 <fkmuiruri8@gmail.com>
2023-02-19 00:21:00 -0800

    Edit jon

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- list all cities contained in db hbtn_0d_usa
 -- display ctiies.id, cities.name, states.name
 -- results sorted in ascending order
-SELECT cities.id, cities.name
+SELECT cities.id, states.name
 FROM cities INNER JOIN states
 ON cities.state_id = states.id
 ORDER BY cities.id;
