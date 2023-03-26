9db132211e876da7d6e898e2eca7de0a97c00206
fk2019 <fkmuiruri8@gmail.com>
2023-03-26 09:07:02 -0700

    Guard empty file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,21 @@
 #!/usr/bin/node
-const args = process.argv;
 const fs = require('fs');
+const args = process.argv;
 if (args.length === 5) {
-  const readStream1 = fs.createReadStream(args[2]);
-  const readStream2 = fs.createReadStream(args[3]);
-  const writeStream = fs.createWriteStream(args[4]);
-  readStream1.pipe(writeStream);
-  readStream2.pipe(writeStream);
+  fs.stat(args[2], (error, stats) => {
+    if (error) {
+      console.error(error);
+    }
+    if (stats.size === 0) {
+      const readStream2 = fs.createReadStream(args[3]);
+      const writeStream = fs.createWriteStream(args[4]);
+      readStream2.pipe(writeStream);
+    } else {
+      const readStream1 = fs.createReadStream(args[2]);
+      const readStream2 = fs.createReadStream(args[3]);
+      const writeStream = fs.createWriteStream(args[4]);
+      readStream1.pipe(writeStream);
+      readStream2.pipe(writeStream);
+    }
+  });
 }
