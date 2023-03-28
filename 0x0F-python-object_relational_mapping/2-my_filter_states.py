b4f138c92a0e3d91c83e4fdaa26a51ee4b0fb9e6
fk2019 <fkmuiruri8@gmail.com>
2023-03-28 05:27:25 -0700

    Add 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Filter states by input
+"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    user_input = sys.argv[4]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    qry = """
+    SELECT * FROM states WHERE states.name = '{user_input}'
+    ORDER BY id ASC
+    """
+    qry = qry.format(user_input=user_input)
+    cur.execute(qry)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
