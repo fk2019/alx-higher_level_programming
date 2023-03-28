b808f6a6edfaeeaa12ef53c11e4467a0ff8b4ef1
fk2019 <fkmuiruri8@gmail.com>
2023-03-28 05:45:39 -0700

    Edit class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Class links itsel to a table in db"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
