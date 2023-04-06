f616a6cefc7e067888b00565e6aae86646a163c3
fk2019 <fkmuiruri8@gmail.com>
2023-04-06 09:43:51 -0700

    check for errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,14 +2,22 @@
 """Fetch url and display a customized body
 """
 import urllib.request
+from urllib.error import URLError
 
-
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
-    body = res.read()
-    body_type = type(body)
-    msg = res.msg
-
-print("Body response:")
-print(f"\t- type: {body_type}")
-print(f"\t- content: {body}")
-print(f"\t- utf8 content: OK")
+try:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        body = res.read()
+        body_type = type(body)
+        msg = res.msg
+except URLError as e:
+    if hasattr(e, 'reason'):
+        print('We failed to reach a server.')
+        print('Reason: ', e.reason)
+    elif hasattr(e, 'code'):
+        print('The server couldn\'t fulfill the request.')
+        print('Error code: ', e.code)
+else:
+    print("Body response:")
+    print(f"\t- type: {body_type}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: OK")
