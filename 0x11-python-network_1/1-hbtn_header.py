d8f8bb0c92f3224e4e9bbf8e681575ef401571a3
fk2019 <fkmuiruri8@gmail.com>
2023-04-06 10:01:30 -0700

    Make it a func

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,16 +5,24 @@ import sys
 import urllib.request
 from urllib.error import URLError
 
-try:
-    with urllib.request.urlopen(sys.argv[1]) as res:
-        header = res.headers
-        x_id = header["X-Request-Id"]
-except URLError as e:
-    if hasattr(e, 'reason'):
-        print('We failed to reach a server.')
-        print('Reason: ', e.reason)
-    elif hasattr(e, 'code'):
-        print('The server couldn\'t fulfill the request.')
-        print('Error code: ', e.code)
-else:
-    print(x_id)
+
+def get_x_id():
+    """function to get x-request-id
+    """
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as res:
+            header = res.headers
+            x_id = header["X-Request-Id"]
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            print('We failed to reach a server.')
+            print('Reason: ', e.reason)
+        elif hasattr(e, 'code'):
+            print('The server couldn\'t fulfill the request.')
+            print('Error code: ', e.code)
+    else:
+        print(x_id)
+
+
+if __name__ == "__main__":
+    get_x_id()
