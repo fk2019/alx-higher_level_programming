65184587545f9d1e46d45b91dc3d506dd028be60
fk2019 <fkmuiruri8@gmail.com>
2023-04-06 09:54:40 -0700

    Add sys

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Send URL request and display value of `X-Request-Id` of the header
 """
+import sys
 import urllib.request
 from urllib.error import URLError
 
 try:
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
-        header  = res.headers
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        header = res.headers
         x_id = header["X-Request-Id"]
 except URLError as e:
     if hasattr(e, 'reason'):
