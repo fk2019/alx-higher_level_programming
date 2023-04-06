db4a00b852157872698d59afddbbfdb97fac3a17
fk2019 <fkmuiruri8@gmail.com>
2023-04-06 05:14:48 -0700

    Ensure 3 lines

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # respond with custom message in response body
-message="You got me!"
-curl -sw "$message" 0.0.0.0:5000/catch_me -o /dev/null
+curl -sw "You got me!" 0.0.0.0:5000/catch_me -o /dev/null
