13d4ce87c68143f94b5108900016b9ff6c4c822b
fk2019 <fkmuiruri8@gmail.com>
2023-04-08 01:04:29 -0700

    Add header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Make GET request using request and sys modules
+"""
+import requests
+import sys
+
+
+def header():
+    """Use requests module to make request and return X-Request-Id
+       value
+    """
+    url = sys.argv[1]
+    req = requests.get(url, auth=('user', 'pass'))
+    head = req.headers
+    x_req_id = head['X-Request-Id']
+    print(x_req_id)
+
+
+if __name__ == "__main__":
+    header()
