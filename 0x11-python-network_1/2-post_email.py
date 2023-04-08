2c84622923a96807e5c739be2a026b1c0a8b0b6b
fk2019 <fkmuiruri8@gmail.com>
2023-04-07 23:51:08 -0700

    Add post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Send POST request to provided URL
+"""
+from urllib import request, parse
+import sys
+
+
+def post():
+    """Make POST req and send email address then return an utf-8
+       body response
+    """
+    url = sys.argv[1]
+    email = sys.argv[2]
+    email = email.encode('ascii')
+    req = request.Request(url, email)
+    with request.urlopen(req) as res:
+        body = res.read()
+        body = body.decode('utf-8')
+    print(body)
+
+
+if __name__ == "__main__":
+    post()
