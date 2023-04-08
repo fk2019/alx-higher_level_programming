33051a27f26779e0652edf026a18b10f6e67bab1
fk2019 <fkmuiruri8@gmail.com>
2023-04-07 23:56:54 -0700

    Add error checks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
 """Send POST request to provided URL
 """
-from urllib import request, parse
 import sys
+from urllib import request, parse
+from urllib.error import URLError
 
 
 def post():
     """Make POST req and send email address then return an utf-8
        body response
     """
-    url = sys.argv[1]
-    email = sys.argv[2]
-    email = email.encode('ascii')
-    req = request.Request(url, email)
-    with request.urlopen(req) as res:
-        body = res.read()
-        body = body.decode('utf-8')
-    print(body)
+    try:
+        url = sys.argv[1]
+        email = sys.argv[2]
+        email = email.encode('ascii')
+        req = request.Request(url, email)
+        with request.urlopen(req) as res:
+            body = res.read()
+            body = body.decode('utf-8')
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            print('We failed to reach a server.')
+            print('Reason: ', e.reason)
+        elif hasattr(e, 'code'):
+            print('The server couldn\'t fulfill the request.')
+            print('Error code: ', e.code)
+        else:
+            print(body)
 
 
 if __name__ == "__main__":
