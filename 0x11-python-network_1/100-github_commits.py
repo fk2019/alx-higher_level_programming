a8464a6146363ca2d4df03daf9ad88a41cc3850c
fk2019 <fkmuiruri8@gmail.com>
2023-04-08 03:24:31 -0700

    Add github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Dsiplay commits by sha and author
+"""
+import requests
+import sys
+
+
+def github_commits():
+    """Display last ten commits by sha and author
+    """
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    api = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    req = requests.get(api)
+    sha = req.json()
+    commits = []
+    for i in sha:
+        commits.append(i)
+    new = commits[-10:]
+    for i in new:
+        print(f"{i['sha']}: {i['commit']['author']['name']}")
+
+
+if __name__ == "__main__":
+    github_commits()
