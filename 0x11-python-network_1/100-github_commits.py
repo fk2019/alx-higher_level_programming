5002250e92fa86a073954749c13e25b6bd9e7c33
fk2019 <fkmuiruri8@gmail.com>
2023-04-08 03:37:24 -0700

    edit for wrong input

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,15 +11,17 @@ def github_commits():
     repo = sys.argv[1]
     owner = sys.argv[2]
     api = f'https://api.github.com/repos/{owner}/{repo}/commits'
-    req = requests.get(api)
-    sha = req.json()
-    commits = []
-    for i in sha:
-        commits.append(i)
-    new = commits[-10:]
-    for i in new:
-        print(f"{i['sha']}: {i['commit']['author']['name']}")
-
+    try:
+        req = requests.get(api)
+        sha = req.json()
+        commits = []
+        for i in sha:
+            commits.append(i)
+        new = commits[-10:]
+        for i in new:
+            print(f"{i['sha']}: {i['commit']['author']['name']}")
+    except:
+        pass
 
 if __name__ == "__main__":
     github_commits()
