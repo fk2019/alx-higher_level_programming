696547c04bdb20caf5bbd4a6c833848874d3a8a1
fk2019 <fk2019@gmail.com>
2023-05-05 20:16:45 +0300

    Inlude args for filename

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const string = process.argv[2];
-const file = 'my_file.txt';
+const string = process.argv[3];
+const file = process.argv[2];
 fs.writeFile(file, string, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
