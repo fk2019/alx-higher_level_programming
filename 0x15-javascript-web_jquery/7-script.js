9ccc829f27732df8f9da00b13d495a46e53207e9
fk2019 <fk2019@gmail.com>
2023-05-06 16:11:03 +0300

    Add 7-script

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+$(function () {
+  const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
+  $.get(url, (data) => {
+    const name = data.name;
+    $('#character').text(name);
+  });
+});
